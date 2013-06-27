fd9e521247fde2abe7052be1e595396a10503683
Walter P Babic <wbabic@mac.com>
2013-06-27 15:56:41 -0600

    upgraded friend version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject friend-test "0.1.0-SNAPSHOT"
+(defproject friend-test "0.1.1"
   :description "take authentication using friend out for a spin"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.cemerick/friend "0.1.2" :exclusions [ring/ring-core]]
+                 [com.cemerick/friend "0.1.4" :exclusions [ring/ring-core]]
                  [compojure "1.1.3"]
                  [clj-http "0.5.5"]
                  [hiccup "1.0.1"]
                  [ring-mock "0.1.3"]]
-  :plugins [[lein-ring "0.7.5"]]
+  :plugins [[lein-ring "0.8.2"]]
   :ring {:handler friend-test.core/secured-app})
